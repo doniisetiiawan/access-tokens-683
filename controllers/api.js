ed232515dbca67fbd58e6522682a5caf140f1248
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-03 15:26:19 +0700

    Refactor api

--- controllers/api.js
+++ controllers/api.js
@@ -1,58 +1,106 @@
 import express from 'express';
+import {
+  createPost,
+  findAllPosts,
+  findBySlug,
+  removePost,
+  updatePost,
+} from './blog';
 
 const router = express.Router();
-
-const posts = [
-  {
-    id: 1,
-    title: 'My blog post 1',
-    content: '<p>Content</p>',
-    author: 'Carlos Santana',
-  },
-  {
-    id: 2,
-    title: 'My blog post 2',
-    content: '<p>Content</p>',
-    author: 'Cristina Rojas',
-  },
-  {
-    id: 3,
-    title: 'My blog post 3',
-    content: '<p>Content</p>',
-    author: 'Carlos Santana',
-  },
-];
 
 router.get('/', (req, res) => {
   res.send(`
     <p>API Endpoints:</p>
     <ul>
-      <li>/api/posts</li>
-      <li>/api/post/:id</li>
+      <li><a href="/api/posts">/api/posts</a></li>
+      <li><a href="/api/post/1">/api/post/:id</a></li>
     </ul>
 `);
 });
 
 router.get('/posts', (req, res) => {
-  res.json({
-    response: posts,
+  findAllPosts((posts) => {
+    res.json({
+      response: posts,
+    });
   });
 });
 
-router.get('/post/:id', (req, res) => {
-  const { params: { id } } = req;
+router.get('/post/:slug', (req, res) => {
+  const { params: { slug } } = req;
 
-  const singlePost = posts.find(post => post.id === Number(id));
+  findBySlug(slug, (singlePost) => {
+    console.log('single', singlePost);
+    if (!singlePost || singlePost.length === 0) {
+      res.send({
+        error: true,
+        message: 'Post not found',
+      });
+    } else {
+      res.json({
+        response: [singlePost],
+      });
+    }
+  });
+});
 
-  if (!singlePost) {
-    res.send({
-      error: true,
-      message: 'Post not found',
-    });
-  }
+router.post('/post', (req, res) => {
+  const { title, content } = req.body;
 
-  res.json({
-    response: [singlePost],
+  createPost(title, content, (data, error = false) => {
+    if (error) {
+      res.json({
+        error: true,
+        message: data,
+      });
+    } else {
+      res.json({
+        response: {
+          saved: true,
+          post: data,
+        },
+      });
+    }
+  });
+});
+
+router.delete('/post/:slug', (req, res) => {
+  const { params: { slug } } = req;
+
+  removePost(slug, (removed, error) => {
+    if (error) {
+      res.json({
+        error: true,
+        message: 'There was an error trying to remove this post...',
+      });
+    } else {
+      res.json({
+        response: {
+          removed: true,
+        },
+      });
+    }
+  });
+});
+
+router.put('/post/:slug', (req, res) => {
+  const { params: { slug }, body: { title, content } } = req;
+
+  updatePost(slug, title, content, (affected, error) => {
+    if (error) {
+      res.json({
+        error: true,
+        message: 'There was an error trying to update the post',
+      });
+    } else {
+      res.json({
+        response: {
+          updated: true,
+          affected,
+        },
+      });
+    }
   });
 });
 
