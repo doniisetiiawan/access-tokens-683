228aba95ec6fedea8f3ba935cb71201b059f4518
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-03 15:04:47 +0700

    Refactor app

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 import apiController from './controllers/api';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost/blog');
 
 app.use('/api', apiController);
 
