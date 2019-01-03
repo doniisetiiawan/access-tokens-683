151742cb6261d51865f11d5f2d6bb3e3a86a74ec
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-03 15:05:10 +0700

    Add blog model

---
+++ models/blog.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+import slug from 'slug';
+
+const postSchema = new Schema({
+  title: String,
+  slug: { type: String, unique: true },
+  content: { type: String, required: true },
+  author: String,
+  createdAt: Date,
+});
+
+postSchema.methods.addAuthor = function (author) {
+  this.author = author;
+
+  return this.author;
+};
+
+postSchema.pre('save', function (next) {
+  this.slug = slug(this.title, { lower: 'on' });
+  this.createdAt = Date.now();
+
+  next();
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
